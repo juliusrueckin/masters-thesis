bba595a5ff72575554b9a96036aa3de9c29cf587
Julius Rueckin <julius.rueckin@gmail.com>
2020-09-24 13:35:01 +0200

    refactor dynamic system and partition related functions in own classes

---
+++ experimental/utils/dynamic_system.py
@@ -0,0 +1,35 @@
+import numpy as np
+from scipy import optimize
+
+
+class DynamicSystem:
+    def __init__(self, phi: callable, d_phi: callable, m_id: float):
+        self.phi = phi
+        self.d_phi = d_phi
+        self.m_id = m_id
+        self.fixed_point = None
+
+    def identification_occurs(self, x: np.array) -> bool:
+        return np.any(x != x % self.m_id)
+
+    def fixed_point_is_hyperbolic(self) -> bool:
+        eig_vals = np.linalg.eigvals(self.d_phi(self.fixed_point))
+        return np.all(eig_vals != 1)
+
+    def is_fixed_point(self, x: np.array):
+        return np.allclose(x, self.phi(x), atol=10e-8)
+
+    def fixed_point_objective_func(self, x: np.array) -> np.array:
+        return self.phi(x) - x
+
+    def fixed_point_jacobian(self, x: np.array) -> np.array:
+        return self.d_phi(x) - np.identity(n=2)
+
+    def compute_fixed_point(self, init_x: np.array=np.array([1, 1])) -> np.array:
+        root_sol = optimize.root(self.fixed_point_objective_func, init_x, jac=self.fixed_point_jacobian, method="lm")
+        if not root_sol.success:
+            print(f"Failed to find a fixed point. Error: {root_sol.message}")
+            return None
+
+        self.fixed_point = root_sol.x
+        return self.fixed_point
---
+++ experimental/utils/partition.py
@@ -0,0 +1,122 @@
+from typing import Union, Optional
+
+import matplotlib.pyplot as plt
+import numpy as np
+from shapely.geometry import MultiLineString, Point, MultiPoint
+from utils.dynamic_sytem import DynamicSystem
+
+
+class Partition:
+    def __init__(self, dynamic_system: DynamicSystem):
+        self.dynamic_system = dynamic_system
+
+    def compute_unstable_eigenspace(self, x: np.array):
+        eig_vals, eig_vects = np.linalg.eig(self.dynamic_system.d_phi(x))
+        eig_vects = np.transpose(eig_vects)
+        msk = eig_vals > 1
+        return eig_vects[msk][0]
+
+    def compute_stable_eigenspace(self, x: np.array):
+        eig_vals, eig_vects = np.linalg.eig(self.dynamic_system.d_phi(x))
+        eig_vects = np.transpose(eig_vects)
+        msk = eig_vals < 1
+        return eig_vects[msk][0]
+
+    def wt_s(self, last_wt_s: np.array, delta: float=10e-3, reverse_direction: bool=False) -> np.array:
+        direction_sign = 1 if not reverse_direction else -1
+        return last_wt_s + direction_sign * delta * self.compute_stable_eigenspace(last_wt_s)
+
+    def wt_u(self, last_wt_u: np.array, delta: float=10e-3, reverse_direction: bool=False) -> np.array:
+        direction_sign = 1 if not reverse_direction else -1
+        return last_wt_u + direction_sign * delta * self.compute_unstable_eigenspace(last_wt_u)
+
+    @staticmethod
+    def get_num_of_intersections(intersection_points: Union[Point, MultiPoint]) -> int:
+        if intersection_points.is_empty:
+            return 0
+        elif isinstance(intersection_points, Point):
+            return 1
+        else:
+            return np.array(intersection_points).shape[0]
+
+    @staticmethod
+    def get_new_intersection_point(intersection_points: np.array, num_intersections: int, overall_intersection_points: np.array) -> Optional[np.array]:
+        if num_intersections == 1:
+            return intersection_points
+
+        msk = np.ones(intersection_points.shape[0], dtype=bool)
+        for j in range(intersection_points.shape[0]):
+            for k in range(overall_intersection_points.shape[0]):
+                if np.allclose(intersection_points[j, :], overall_intersection_points[k]):
+                    msk[j] = False
+                    break
+
+        if msk.sum() != 1:
+            return None
+
+        return intersection_points[msk][0]
+
+    def compute_partition(self, fixed_point: np.array, num_iters: int, delta: float):
+        branches = {"W_u1": [[fixed_point]], "W_u2": [[fixed_point]], "W_s1": [[fixed_point]], "W_s2": [[fixed_point]]}
+        unstable_branches = ["W_u1", "W_u2"]
+        stable_branches = ["W_s1", "W_s2"]
+        approx_funcs = {"W_u1": (self.wt_u, True), "W_u2": (self.wt_u, False), "W_s1": (self.wt_s, True), "W_s2": (self.wt_s, False)}
+        overall_intersection_points = []
+
+        for i in range(num_iters):
+            print(f"ITERATION: {i+1}")
+            stopped_branches = []
+            trace_branches = list(branches.keys())
+            while len(stopped_branches) < len(branches.keys()):
+                trace_branches = [branch for branch in trace_branches if branch not in stopped_branches]
+                for branch_key in trace_branches:
+                    last_wt = branches[branch_key][-1][-1]
+                    approx_func, rev_dir = approx_funcs[branch_key]
+                    new_wt = approx_func(last_wt, self.dynamic_system.d_phi, delta=delta, reverse_direction=rev_dir)
+
+                    if not self.dynamic_system.identification_occurs(new_wt):
+                        branches[branch_key][-1].append(new_wt)
+                    else:
+                        if len(branches[branch_key][-1]) == 1:
+                            branches[branch_key][-1].append(branches[branch_key][-1][0])
+
+                        new_wt = new_wt % self.dynamic_system.m_id
+                        branches[branch_key].append([new_wt, new_wt])
+
+                for unstable_branch in unstable_branches:
+                    for stable_branch in stable_branches:
+                        if unstable_branch in stopped_branches and stable_branch in stopped_branches:
+                            continue
+                        intersection_points = MultiLineString(branches[unstable_branch]).intersection(MultiLineString(branches[stable_branch]))
+                        intersection_points = intersection_points.difference(Point(fixed_point))
+                        num_intersections = self.get_num_of_intersections(intersection_points)
+
+                        if num_intersections > i:
+                            intersection_point = self.get_new_intersection_point(np.array(intersection_points), num_intersections, np.array(overall_intersection_points))
+                            if intersection_point is None:
+                                    continue
+
+                            dist_unstable = np.linalg.norm(np.array(intersection_point) - branches[unstable_branch][-1][-1], ord=2)
+                            dist_stable = np.linalg.norm(np.array(intersection_point) - branches[stable_branch][-1][-1], ord=2)
+                            latter_branch = unstable_branch if dist_unstable < dist_stable else stable_branch
+                            if latter_branch not in stopped_branches:
+                                stopped_branches.append(latter_branch)
+                                print(f"Stop {latter_branch} at {intersection_point}.")
+                                overall_intersection_points.append(intersection_point)
+        return branches, overall_intersection_points
+
+    def plot_partition(self, branches, intersection_points):
+        m_id = self.dynamic_system.m_id
+        unit_square = np.array([[0,0], [0,m_id], [m_id,m_id], [m_id,0], [0,0]])
+        plt.plot(unit_square[:, 0], unit_square[:, 1], "r-")
+
+        for branch in branches.keys():
+            for line in MultiLineString(branches[branch]):
+                plt.plot(np.array(line.coords[:])[:, 0], np.array(line.coords[:])[:, 1], "b-")
+
+        plt.plot(self.dynamic_system.fixed_point[0], self.dynamic_system.fixed_point[1], "yo")
+
+        intersection_points = np.array(intersection_points)
+        plt.plot(intersection_points[:, 0], intersection_points[:, 1], "go")
+
+        plt.show()
