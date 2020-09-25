de609b6b413ee0211b0f54649dc402012482b206
Julius Rueckin <julius.rueckin@gmail.com>
2020-09-25 15:35:23 +0200

    add documentation to dynamic system and partiton wrapper

--- experimental/utils/dynamic_system.py
+++ experimental/utils/dynamic_system.py
@@ -3,32 +3,105 @@ from scipy import optimize
 
 
 class DynamicSystem:
+    """
+    DaynmicSystem class is a wrapper the system dynamics, its Jacobian and functions to compute hyperbolic fixed
+    points of the system.
+    """
+
     def __init__(self, phi_non_identified: callable, d_phi: callable, m_id: float):
+        """
+        Args:
+            phi_non_identified (callable): function that implements the system dynamics/ the system's flow
+            d_phi (callable): function that implements the Jacobian of the systems flow
+            m_id (float): torus identification constant (x mod m_id) defining the cosets of x
+        """
         self.phi_non_identified = phi_non_identified
         self.d_phi = d_phi
         self.m_id = m_id
         self.fixed_point = None
 
     def phi(self, x: np.array) -> np.array:
+        """
+        Implements the system dynamics over the torus, hence evaluates the system's flow mod m_id.
+
+        Args:
+            x: point in system's phase space
+
+        Returns:
+            (np.array): point in phase space under application of system's flow
+        """
         return self.phi_non_identified(x) % self.m_id
 
     def identification_occurs(self, x: np.array) -> bool:
+        """
+        Checks if identification of x with a representative of its torus coset occurs.
+
+        Args:
+            x (np.array): point in euclidean plane
+
+        Returns:
+            (bool): indicates, if identification of x with coset occurs
+        """
         return np.any(x != x % self.m_id)
 
     def fixed_point_is_hyperbolic(self) -> bool:
+        """
+        Checks if system's fixed point is hyperbolic, i.e. if absolute values of eigenvalues of Jacobian in fixed
+        point are != 1.
+
+        Returns:
+            (bool): indicates if computed fixed point is a hyperbolic one
+        """
         eig_vals = np.linalg.eigvals(self.d_phi(self.fixed_point))
         return np.all(eig_vals != 1)
 
     def is_fixed_point(self, x: np.array):
+        """
+        Checks if a point x in the phase space is a fixed point, i.e. if phi(x) = x.
+
+        Args:
+            x (np.array): point in phase space
+
+        Returns:
+            (bool): indicates if a given point x is a fixed point
+        """
         return np.allclose(x, self.phi(x), atol=10e-8)
 
     def fixed_point_objective_func(self, x: np.array) -> np.array:
+        """
+        Implements objective function for applying Levenberg-Marquardt to find fixed point, i.e. phi(x) -x.
+
+        Args:
+            x (np.array): point in phase space
+
+        Returns:
+            (np.array): value of objective function evaluated at x
+        """
         return self.phi_non_identified(x) - x
 
     def fixed_point_jacobian(self, x: np.array) -> np.array:
+        """
+        Implements Jacobian of objective function to compute fixed point, i.e. D_phi(x) - I.
+
+        Args:
+            x (np.array): point in phase space
+
+        Returns:
+            (np.array): Jacobian of objective function evaluated at x
+        """
         return self.d_phi(x) - np.identity(n=2)
 
     def compute_fixed_point(self, init_x: np.array = np.array([4, 4])) -> np.array:
+        """
+        Computes fixed point of system dynamics via Levenberg-Marquardt algorithm that solves root finding
+        problem phi(x) - x = 0, whch is equivalent to finding a fixed point phi(x) = x.
+
+        Args:
+            init_x (np.array): point in phase space in which we start the Levenberg-Marquardt algorithm
+
+        Returns:
+            (np.array): iteratively calculated fixed point of system dynamics
+        """
         root_sol = optimize.root(self.fixed_point_objective_func, init_x, jac=self.fixed_point_jacobian, method="lm")
         if not root_sol.success:
             print(f"Failed to find a fixed point. Error: {root_sol.message}")
--- experimental/utils/partition.py
+++ experimental/utils/partition.py
@@ -7,33 +7,103 @@ from shapely.geometry import MultiLineString, Point, MultiPoint
 
 
 class Partition:
+    """
+    Partition class is a wrapper for all algorithmic steps required to compute a Markov partition based on a given
+    dynamic system. This primarily includes the construction of a Markov partition by tracing stable and unstable
+    manifolds in a hyperbolic fixed point of the given dynamic system and its helper functions described in chapter
+    4.1. of the master's thesis.
+    """
+
     def __init__(self, dynamic_system: DynamicSystem):
+        """
+        Args:
+            dynamic_system (DynamicSystem): including functionality for calculating system dynamics and fixed points.
+        """
         self.dynamic_system = dynamic_system
         self.branches = None
         self.intersection_points = None
 
     def compute_unstable_eigenspace(self, x: np.array):
+        """
+        Computes the unstable eigenspace in x that describes the local development of the unstable manifold.
+        The unstable eigenspace in x is given by the eigenvector of the Jacobian of the system dynamics in x
+        that corresponds to the largest eigenvalue.
+
+        Args:
+            x (np.array): point in the phase space of the system
+
+        Returns:
+            (np.array): unstable eigenvector of the Jacobian of the system dynamics in x
+        """
         eig_vals, eig_vects = np.linalg.eig(self.dynamic_system.d_phi(x))
         eig_vects = np.transpose(eig_vects)
         msk = eig_vals > 1
         return eig_vects[msk][0]
 
     def compute_stable_eigenspace(self, x: np.array):
+        """
+        Computes the stable eigenspace in x that describes the local development of the stable manifold.
+        The stable eigenspace in x is given by the eigenvector of the Jacobian of the system dynamics in x
+        that corresponds to the smallest eigenvalue.
+
+        Args:
+            x (np.array): a point in the phase space of the system
+
+        Returns:
+            (np.array): stable eigenvector of the Jacobian of the system dynamics in x
+        """
         eig_vals, eig_vects = np.linalg.eig(self.dynamic_system.d_phi(x))
         eig_vects = np.transpose(eig_vects)
         msk = eig_vals < 1
         return eig_vects[msk][0]
 
     def wt_s(self, last_wt_s: np.array, delta: float = 10e-3, reverse_direction: bool = False) -> np.array:
+        """
+        Approximates the next point on the stable manifold given the last approximated point on this manifold
+        by computing the stable eigenspace in the last approximated point and integrating the next point based
+        on this stable eigenspace with a sufficiently small integration constant delta.
+
+        Args:
+            last_wt_s (np.array): last approximated point on the stable manifold
+            delta (float): integration constant that defines the step size of the next point on the manifold
+            reverse_direction (bool): defines in which direction the branch of the manifold should flow
+
+        Returns:
+            (np.array): approximated next point on the stable manifold
+        """
         direction_sign = 1 if not reverse_direction else -1
         return last_wt_s + direction_sign * delta * self.compute_stable_eigenspace(last_wt_s)
 
     def wt_u(self, last_wt_u: np.array, delta: float = 10e-3, reverse_direction: bool = False) -> np.array:
+        """
+        Approximates the next point on the unstable manifold given the last approximated point on this manifold
+        by computing the unstable eigenspace in the last approximated point and integrating the next point based
+        on this unstable eigenspace with a sufficiently small integration constant delta.
+
+        Args:
+            last_wt_u (np.array): last approximated point on the unstable manifold
+            delta (float): integration constant that defines the step size of the next point on the manifold
+            reverse_direction (bool): defines in which direction the branch of the manifold should flow
+
+        Returns:
+            (np.array): approximated next point on the unstable manifold
+        """
         direction_sign = 1 if not reverse_direction else -1
         return last_wt_u + direction_sign * delta * self.compute_unstable_eigenspace(last_wt_u)
 
     @staticmethod
     def get_num_of_intersections(intersection_points: Union[Point, MultiPoint]) -> int:
+        """
+        Return number of points in intersection_points shapely data structure. Since shapely returns three
+        different data types for none, one or more intersection points, we directly need to evaluate differently
+        for all three cases.
+
+        Args:
+            intersection_points (Point or MultiPoint): point(s) of intersection(s) as shapely data structure
+
+        Returns:
+            (int): number of points in intersection_points data structure
+        """
         if intersection_points.is_empty:
             return 0
         elif isinstance(intersection_points, Point):
@@ -45,6 +115,20 @@ class Partition:
     def get_new_intersection_point(
         intersection_points: np.array, num_intersections: int, overall_intersection_points: np.array
     ) -> Optional[np.array]:
+        """
+        Selects the potential new intersection point between two branches in intersection_points over all already
+        computed intersection points in overall_intersection_points. This needs special treatment, since shapely
+        does not sort its intersection points and moreover since we approximate branches, small numerical
+        differences make it difficult to directly compare intersection points.
+
+        Args:
+            intersection_points (np.array): intersection points between two branches
+            num_intersections (int): number of points in intersection_points
+            overall_intersection_points (np.array): intersection points between all branches
+
+        Returns:
+            (np.array): new intersection point between two branches, if it exists
+        """
         if num_intersections == 1:
             return intersection_points
 
@@ -61,6 +145,20 @@ class Partition:
         return intersection_points[msk][0]
 
     def compute_partition(self, num_iters: int, delta: float) -> Tuple[Optional[Dict], Optional[List]]:
+        """
+        Computes a Markov partition for the given dynamic system by tracing stable and unstable manifolds in
+        the hyperbolic fixed point in parallel and checking for their points of intersection. This procedure
+        is repeated num_iters times until the partition is sufficiently fine enough to be Markov. All in all,
+        this function implements the described algorithm in chapter 4.2 of the master's thesis.
+
+        Args:
+            num_iters (int): number of repetitions for tracing branches until points of intersections
+            delta (float): sufficiently small integration constant for approximating branches
+
+        Returns:
+            (dict): keys are branch identifiers with lists of points as values
+            (list): list of points of intersections between branches
+        """
         if self.dynamic_system.fixed_point is None:
             if self.dynamic_system.compute_fixed_point() is None:
                 print(f"Failed calculating a partition, since no fixed point found.")
@@ -144,17 +242,24 @@ class Partition:
         return self.branches, self.intersection_points
 
     def plot_partition(self):
+        """
+        Plots square with side length of magnitude identical to the identification constants of the cosets that
+        make up the torus of the dynamic system. If already calculated, it also plots the stable and unstable
+        branches of the Markov partition of the given dynamic system as well as the branch intersection points
+        and the dynamic system's hyperbolic fixed point.
+        """
         m_id = self.dynamic_system.m_id
         unit_square = np.array([[0, 0], [0, m_id], [m_id, m_id], [m_id, 0], [0, 0]])
         plt.plot(unit_square[:, 0], unit_square[:, 1], "r-")
 
-        for branch in self.branches.keys():
-            for line in MultiLineString(self.branches[branch]):
-                plt.plot(np.array(line.coords[:])[:, 0], np.array(line.coords[:])[:, 1], "b-")
+        if self.branches is not None and self.intersection_points is not None:
+            for branch in self.branches.keys():
+                for line in MultiLineString(self.branches[branch]):
+                    plt.plot(np.array(line.coords[:])[:, 0], np.array(line.coords[:])[:, 1], "b-")
 
-        plt.plot(self.dynamic_system.fixed_point[0], self.dynamic_system.fixed_point[1], "yo")
+            plt.plot(self.dynamic_system.fixed_point[0], self.dynamic_system.fixed_point[1], "yo")
 
-        intersection_points = np.array(self.intersection_points)
-        plt.plot(intersection_points[:, 0], intersection_points[:, 1], "go")
+            intersection_points = np.array(self.intersection_points)
+            plt.plot(intersection_points[:, 0], intersection_points[:, 1], "go")
 
         plt.show()
